77de5bd5157f230e84eaf518b7bed33a2965af2a
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-03-14 17:15:38 -0500

    Update LargestSumSubArray.java

--- LargestSumSubArray.java
+++ LargestSumSubArray.java
@@ -35,4 +35,18 @@ public class LargestSumSubArray {
 		System.out.println("Indexes (" + maxStart + ", " + maxEnd + ")");
 	}
 }
+/*
+//Problem
 
+Given an integer array find the subarray which has the largest sum.
+show code
+
+Solution
+
+We will keep two pointers at the start of the array. 
+We will keep on incrementing the end pointer and calculate the current sum. 
+If at any index current sum is larger than the maximum sum we will update the maximum sum and the corresponding indexes. 
+If the current sum is zero or negative at any point, that means current subarray will not be a part of the maximum sum subset. 
+In this scenario we move both the pointer to the next index and continue the process. 
+The complexity of this algorithm is O(n) because the while loop can be looped at most 2*n times
+*/
