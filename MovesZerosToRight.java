54d030396267ccabe15a1b745c58b0c89237135e
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-03-23 15:05:33 -0500

    Update MovesZerosToRight.java

--- MovesZerosToRight.java
+++ MovesZerosToRight.java
@@ -33,4 +33,20 @@ public class MovesZerosToRight {
 		}
 	}
 	
+	
 }
+/*
+Problem
+
+You are given an integer array which contains some zeros. 
+Move the zeros to the right side of the array with minimum number of swaps. 
+The order of the original array can be destroyed.
+
+Solution
+
+We can do this in at most n/2 swaps. We move one index from left to right side and another from right to left side. 
+If we get a non zero number at right index and zero in the left index then we swap the numbers. 
+If in the right side we get a zero we just move the pointer to the left side and if we get a non zero in 
+the left index then we move it to the right side. 
+We continue this process till the two pointers meet.
+*/
