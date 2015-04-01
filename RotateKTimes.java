22f5ac017de3d90346442d855280cb51fdf9fb02
Brijesh Patel <Brijeshrpatel9@users.noreply.github.com>
2015-04-01 06:51:52 -0500

    Create RotateKTimes.java

---
+++ RotateKTimes.java
@@ -0,0 +1,44 @@
+import java.util.Arrays;
+
+public class RotateKTimes
+{
+ public static void main(String[] args)
+ {
+  int[] array =
+  { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+  printArray(array);
+  rotateLeftKTimes(array, 3);
+  printArray(array);
+ }
+
+ private static void rotateLeftKTimes(int[] array, int k)
+ {
+  reverse(array, 0, array.length - 1);
+  System.out.println(Arrays.toString(array));
+  reverse(array, 0, array.length - k - 1);
+  System.out.println(Arrays.toString(array));
+  reverse(array, array.length - k, array.length - 1);
+  System.out.println(Arrays.toString(array));
+ }
+
+ private static void reverse(int[] array, int i, int j)
+ {
+  while (i < j)
+  {
+   int temp = array[i];
+   array[i] = array[j];
+   array[j] = temp;
+   i++;
+   j--;
+  }
+ }
+
+ private static void printArray(int[] array)
+ {
+  for (int i : array)
+   System.out.print(i + ",");
+  System.out.println();
+
+ }
+
+}
